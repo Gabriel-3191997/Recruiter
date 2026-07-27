8da539139f4b768fcd417cb2486fe71d9de7c365
Gabriel_31997 <gabrielwkun@gmail.com>
2026-07-27 02:10:19 +0000

    add a new page

--- src/pages/employeer/default.jsx
+++ src/pages/employeer/default.jsx
@@ -5,9 +5,115 @@ class Default extends React.Component {
     return (
       <>
         <div className="flex flex-col justify-center items-center h-auto mt-24">
-          <h1 className="text-3xl font-sans font-bold capitalize">
+          {/* <h1 className="text-3xl font-sans font-bold capitalize">
             recruiter <br /> profile
-          </h1>
+          </h1> */}
+
+          <div class="border-b border-default">
+            <ul class="flex flex-wrap -mb-px md:gap-28 text-sm md:text-lg font-medium text-center text-body">
+              <li class="me-2">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center p-4 border-b border-transparent rounded-t-base hover:text-fg-brand hover:border-brand group"
+                >
+                  <svg
+                    class="w-4 h-4 me-2 text-body group-hover:text-fg-brand"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                  Profile
+                </a>
+              </li>
+              {/* <li class="me-2">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center p-4 text-fg-brand border-b border-brand rounded-t-base active group"
+                  aria-current="page"
+                >
+                  <svg
+                    class="w-4 h-4 me-2 text-fg-brand"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9.143 4H4.857A.857.857 0 0 0 4 4.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 10 9.143V4.857A.857.857 0 0 0 9.143 4Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 20 9.143V4.857A.857.857 0 0 0 19.143 4Zm-10 10H4.857a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286A.857.857 0 0 0 9.143 14Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286a.857.857 0 0 0-.857-.857Z"
+                    />
+                  </svg>
+                  Dash Board
+                </a>
+              </li> */}
+              <li class="me-2">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center p-4 border-b border-transparent rounded-t-base hover:text-fg-brand hover:border-brand group"
+                >
+                  <svg
+                    class="w-4 h-4 me-2 text-body group-hover:text-fg-brand"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M8 7V5a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2m-8 0H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2M8 7h8"
+                    />
+                  </svg>
+                  Job Descriptions
+                </a>
+              </li>
+              <li class="me-2">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center p-4 border-b border-transparent rounded-t-base hover:text-fg-brand hover:border-brand group"
+                >
+                  <svg
+                    class="w-4 h-4 me-2 text-body group-hover:text-fg-brand"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 5v9m-5 0H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2M8 9l4-5 4 5"
+                    />
+                  </svg>
+                  Publish
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </>
     );
